85757fd9204ceb8b1f9b73b870d39a1e96c4e324
HungTruong4689 <hungphuc4689@gmail.com>
2024-01-22 12:14:05 +0100

    angular module'

--- Angular-Review/angular-first-app/src/app/app.component.ts
+++ Angular-Review/angular-first-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+//Writing an new component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-first-app';
--- Angular-Review/angular-first-app/src/app/app.module.ts
+++ Angular-Review/angular-first-app/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//Angular organizes components into self-containered blcoked of functionality called modules.
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent], //define all components that exist inside the module
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
