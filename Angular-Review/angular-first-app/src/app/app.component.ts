aaf554e8032e242a0a2e336075899ff7148dda20
HungTruong4689 <hungphuc4689@gmail.com>
2024-02-08 20:14:37 +0100

    template html

--- Angular-Review/angular-first-app/src/app/app.component.ts
+++ Angular-Review/angular-first-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 //Writing an new component
 @Component({
   selector: 'app-root', // match HTML tags with components via a selector
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //html
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
