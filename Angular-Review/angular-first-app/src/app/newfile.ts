ff36503ea38a0a61a6d89ca4625fd58f2941d2a1
HungTruong4689 <hungphuc4689@gmail.com>
2024-02-08 20:36:08 +0100

    create new component'

---
+++ Angular-Review/angular-first-app/src/app/newfile.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+//a-component to create files component
+
+// @Component({
+//   selector: 'selector-name',
+//   templateUrl: 'name.component.html',
+// })
+export class NameComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
