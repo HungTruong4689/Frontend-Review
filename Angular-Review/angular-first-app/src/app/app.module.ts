30dd3dac9ff6f09a991841692523f7823d8bb4e9
HungTruong4689 <hungphuc4689@gmail.com>
2024-02-08 20:11:37 +0100

    angular convention

--- Angular-Review/angular-first-app/src/app/app.module.ts
+++ Angular-Review/angular-first-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 //Angular organizes components into self-containered blcoked of functionality called modules.
+//An Angular application has at least one main module called AppModule, as a convention
 @NgModule({
   declarations: [AppComponent], //define all components that exist inside the module
   imports: [BrowserModule],
