6551808622e1e8173d7ae01dc760572573ce9415
HungTruong4689 <hungphuc4689@gmail.com>
2024-02-09 20:44:23 +0100

    typescript string type

--- Angular-Review/angular-first-app/src/app/newfile.ts
+++ Angular-Review/angular-first-app/src/app/newfile.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 //a-component to create files component
 
-// @Component({
-//   selector: 'selector-name',
-//   templateUrl: 'name.component.html',
-// })
+@Component({
+  selector: 'selector-name',
+  templateUrl: 'name.component.html',
+})
 export class NameComponent implements OnInit {
   constructor() {}
+  brand: string = 'Chervolet'; //string type
 
   ngOnInit() {}
 }
