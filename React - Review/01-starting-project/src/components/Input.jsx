dce8ff38ea2d4d52c0b7b0fe96894005f69c0241
HungTruong4689 <hungphuc4689@gmail.com>
2024-02-15 19:35:35 +0100

    input text

--- React - Review/01-starting-project/src/components/Input.jsx
+++ React - Review/01-starting-project/src/components/Input.jsx
@@ -4,7 +4,7 @@ export default function Input({ label, textarea, ...props }) {
 
     return(
         <p className = "flex flex-col gap-1 my-4">
-            <label>
+            <label className='text-sm font-bold uppercase text-stone-500'>
 
             </label>
             {
@@ -13,16 +13,5 @@ export default function Input({ label, textarea, ...props }) {
         </p>
     )
 
-//   return (
-//     <p className="flex flex-col gap-1 my-4">
-//       <label className="text-sm font-bold uppercase text-stone-500">
-//         {label}
-//       </label>
-//       {textarea ? (
-//         <textarea className={classes} {...props} />
-//       ) : (
-//         <input className={classes} {...props} />
-//       )}
-//     </p>
-//   );
+
 }
