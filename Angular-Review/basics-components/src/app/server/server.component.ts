21f92f996f2b8c1bcdb373ebfeff1ae5281d765c
HungTruong4689 <hungphuc4689@gmail.com>
2024-02-23 12:24:06 +0200

    if directive

--- Angular-Review/basics-components/src/app/server/server.component.ts
+++ Angular-Review/basics-components/src/app/server/server.component.ts
@@ -8,8 +8,14 @@ export class ServerComponent {
   // serverId: number = 10;
   // serverName: string = 'new name';
   userName: string = '';
+  secret: string = 'Luna';
+  showSecret: boolean = false;
 
   resetBtn() {
     this.userName = '';
+  }
+
+  showSecretPass() {
+    this.showSecret = !this.showSecret;
   }
 }
