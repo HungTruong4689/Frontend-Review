0057f4d2da02e6dd1a761ea27f4bbef525b9192d
HungTruong4689 <hungphuc4689@gmail.com>
2024-05-22 12:19:57 +0300

    strategy v2' gi

--- Refactor JS/patterns/strategy.js
+++ Refactor JS/patterns/strategy.js
@@ -1,6 +1,17 @@
 class Person{
+    constructor(knowsBinary){
+        this.knowsBinary = knowsBinary
+    }
     log(number){
         console.log(this.whatIs(number))
+    }
+
+    whatIs(number){
+        if(this.knowsBinary){
+            return Number('0b' + number)
+        }else{
+            return number
+        }
     }
 }
 
