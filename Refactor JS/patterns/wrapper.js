dee5455545693f07904bd9f35a5aad1829e7a217
HungTruong4689 <hungphuc4689@gmail.com>
2024-05-24 10:42:03 +0300

    wrapper v2

--- Refactor JS/patterns/wrapper.js
+++ Refactor JS/patterns/wrapper.js
@@ -17,3 +17,74 @@ test("base dog price", (assert) =>{
     assert.end()
 })
 
+
+function Cute(dog){
+    const cuteDog = Object.create(dog)
+    cuteDog.cost = dog.cost +20
+    return cuteDog
+}
+
+test("cute dog price", (assert) =>{
+    assert.equal((Cute(new Dog)).displayPrice(), 'The dog costs $70')
+    assert.end()
+})
+
+function Trained(dog){
+    const trainedDog = Object.create(dog)
+    trainedDog.cost = dog.cost +20
+    return trainedDog
+}
+
+test("trained/cute dog price", (assert) =>{
+    assert.equal((Trained(Cute(new Dog))).displayPrice(), 'The dog costs $130')
+    assert.end()
+})
+
+class Person{
+    constructor(name){
+        this.name = new NameString(name)
+    }
+}
+
+class AnonymousPerson extends Person{
+    constructor(name){
+        super()
+        this.name = new NameString(name)
+    }
+}
+
+class NameString extends String{
+    capitalize(){
+        return new NameString(this[0].toUpperCase() +this.substring(1))
+    }
+
+    tigerify(){
+        return new NameString(`${this}, the tiger`)
+    }
+
+    display(){
+        return this.toString()
+    }
+}
+
+class NullString{
+    capitalize(){
+        return this
+    }
+    tigerify(){
+        return this
+    }
+
+    display(){
+        return ''
+    }
+}
+
+// this is new
+function WithoutNull(person){
+    personWithoutNull = Object.create(person)
+    if(personWithoutNull.name === null){
+        personWithoutNull.name = NullString
+    }
+    return personWithoutNull
+}
