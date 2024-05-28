d3c3576f6a7dbf783517c012adfe429aeeb4a547
HungTruong4689 <hungphuc4689@gmail.com>
2024-05-28 12:40:07 +0300

    setTimeout

--- Refactor JS/asynchronous/async.js
+++ Refactor JS/asynchronous/async.js
@@ -5,3 +5,23 @@ const response = http.get('https://refactoringjs.com', (result)=>{
     })
 })
 console.log(response.body)
+
+let theResult = []
+http.get('https://refactoringjs.com', (result)=>{
+    result.on('data', (chunk)=>{
+        theResult.push(chunk.toString())
+    })
+})
+console.log(theResult)
+
+setTimeout(()=>{
+    console.log("the chicken")}, 2)
+    console.log("the egg") //print first
+    setTimeout(()=>{
+        console.log("the chicken 2")}, 0)
+        setTimeout(()=>{
+            console.log("the chicken 3")}, 0)
+           
+            setTimeout(()=>{
+                console.log("the chicken 4")}, 0)
+                
